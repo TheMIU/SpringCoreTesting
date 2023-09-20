39f00dd85a093033dbaa81b5c3943a66b5d5d77e
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-09-20 12:53:51 +0530

    Initial commit

---
+++ 01_HelloSpring/src/main/java/lk/ijse/spring/pojo/MyPojo.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyPojo {
+    public MyPojo(){
+        System.out.println("MyPojo Instantiated");
+    }
+}
